ddfa5a4739479fe0c5abcb538937fb960bf0f138
alvar <bienmental22@gmail.com>
2026-04-15 11:05:27 -0600

    Actualiza telefono visible en pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,7 @@ export default function HomePage() {
           <div className="contactStrip">
             <div className="contactStrip__item">
               <svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 10.6 19.79 19.79 0 01.03 2.02 2 2 0 012 0h3a2 2 0 012 1.72c.127.96.361 1.903.7 2.81a2 2 0 01-.45 2.11L6.09 7.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0122 14.92z"/></svg>
-              <a href="tel:+527221234567" className="contactStrip__link">722 123 4567</a>
+              <a href="tel:+5215642275320" className="contactStrip__link">5215642275320</a>
             </div>
             <div className="contactStrip__divider" />
             <div className="contactStrip__item">
